f1fd80785feddbd334632180bda60ec2ed959bd6
Ben Firshman <ben@firshman.co.uk>
2018-05-26 12:55:45 +0300

    Remove strict mode

--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ import registerServiceWorker from "./registerServiceWorker";
 import store from "./store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
