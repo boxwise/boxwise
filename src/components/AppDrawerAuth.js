3a2914dedf970c6b389dcd02263c82e2b55e2c60
Nick Stenning <nick@whiteink.com>
2018-05-27 01:11:49 +0200

    Load and display organisation data
    Add a container component to wrap AppDrawerAuth and load organisations
    data if it exists.
    
    It's likely that the container component really deserves to be a bit
    higher up the tree...

--- src/components/AppDrawerAuth.js
+++ src/components/AppDrawerAuth.js
@@ -1,44 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { compose } from "redux";
-import { connect } from "react-redux";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { isEmpty, withFirebase } from "react-redux-firebase";
 
-const AppDrawerAuth = ({ auth, firebase, organisation }) => {
-  if (isEmpty(auth)) {
-    return null;
-  }
+const AppDrawerAuth = ({ profile, organisation, signOut }) => {
   return (
     <List>
       <ListItem>
-        <ListItemText secondary={`Signed in as ${auth.email}`} />
+        <ListItemText secondary={`Signed in as ${profile.email}`} />
       </ListItem>
       <ListItem>
         <ListItemText secondary={`Organisation ${organisation.id}`} />
       </ListItem>
       <ListItem button>
-        <ListItemText
-          primary="Sign Out"
-          onClick={() => {
-            firebase.auth().signOut();
-          }}
-        />
+        <ListItemText primary="Sign Out" onClick={signOut} />
       </ListItem>
     </List>
   );
 };
 
 AppDrawerAuth.propTypes = {
-  auth: PropTypes.object.isRequired,
-  firebase: PropTypes.object.isRequired
+  profile: PropTypes.object.isRequired,
+  organisation: PropTypes.object.isRequired,
+  signOut: PropTypes.func.isRequired
 };
 
-const mapStateToProps = state => ({
-  auth: state.firebase.auth,
-  organisation: state.organisation
-});
-
-export default compose(connect(mapStateToProps), withFirebase)(AppDrawerAuth);
+export default AppDrawerAuth;
--- src/reducers/organisations.js
+++ src/reducers/organisations.js
@@ -1,25 +1,17 @@
 import { actionTypes } from "react-redux-firebase";
 import { SET_ORGANISATION } from "../actions/organisations";
 
-const initialState = {
-  id: null
-};
+const initialState = null;
 
 export const organisationReducer = (state = initialState, action) => {
   switch (action.type) {
     case SET_ORGANISATION:
-      return {
-        ...state,
-        id: action.organisationId
-      };
+      return action.organisationId;
     // set default organisation
     case actionTypes.SET_PROFILE:
       const organisations = action.profile.organisations;
-      if (!state.organisationId && organisations && organisations.length) {
-        return {
-          ...state,
-          id: organisations[0].id
-        };
+      if (!state && organisations && organisations.length) {
+        return organisations[0];
       }
       return state;
     default:
