a3e674ee9e87c2afd51fff239a2b67ce1d66c1da
Ben Firshman <ben@firshman.co.uk>
2018-05-29 14:49:22 +0300

    Make box table scroll on mobile

--- src/components/BoxTable.js
+++ src/components/BoxTable.js
@@ -1,31 +1,44 @@
 import React from "react";
+import { withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 
-const BoxTable = ({ boxes }) => (
-  <Table>
-    <TableHead>
-      <TableRow>
-        <TableCell>Number</TableCell>
-        <TableCell>Product</TableCell>
-        <TableCell>Count</TableCell>
-        <TableCell>Status</TableCell>
-      </TableRow>
-    </TableHead>
-    <TableBody>
-      {boxes.map(box => (
+const styles = theme => ({
+  table: {
+    minWidth: 700,
+    marginBottom: theme.spacing.unit * 4
+  },
+  tableWrapper: {
+    overflowX: "auto"
+  }
+});
+
+const BoxTable = ({ classes, boxes }) => (
+  <div className={classes.tableWrapper}>
+    <Table className={classes.table}>
+      <TableHead>
         <TableRow>
-          <TableCell>{box.number}</TableCell>
-          <TableCell>{box.product}</TableCell>
-          <TableCell>{box.count}</TableCell>
-          <TableCell>{box.status}</TableCell>
+          <TableCell>Number</TableCell>
+          <TableCell>Product</TableCell>
+          <TableCell>Count</TableCell>
+          <TableCell>Status</TableCell>
         </TableRow>
-      ))}
-    </TableBody>
-  </Table>
+      </TableHead>
+      <TableBody>
+        {boxes.map(box => (
+          <TableRow>
+            <TableCell>{box.number}</TableCell>
+            <TableCell>{box.product}</TableCell>
+            <TableCell>{box.count}</TableCell>
+            <TableCell>{box.status}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </div>
 );
 
-export default BoxTable;
+export default withStyles(styles)(BoxTable);
