ba5fa9983abfb1e3e1e9d3a9854dbcfcc905f51f
Bart Driessen <post@bartdriessen.eu>
2018-05-29 23:19:11 +0300

    New page: create QR labels

--- src/pages/CreateQRLabelsPage.js
+++ src/pages/CreateQRLabelsPage.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "./CreateQRLabelsPage.css";
+import { connect } from "react-redux";
+
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
 
 class CreateQRLabelsPage extends React.Component {
   state = {
@@ -7,13 +16,35 @@ class CreateQRLabelsPage extends React.Component {
   };
 
   render() {
-    let labels = ["foo", "bar"];
+    var i;
+    var labels = [];
+    for (i = 0; i < 100; i++) {
+      labels[i] = i;
+    }
+
     return (
       <div className="CreateQRLabelsPage">
-        {labels.map(label => <p>{label}</p>)}
+        {labels.map(label => (
+          <div className="label">
+            <img
+              src={
+                "https://chart.googleapis.com/chart?cht=qr&chs=150x150&chl=" +
+                uuidv4() +
+                "&chld=L|0"
+              }
+              alt="qr code"
+              className="qrcode"
+            />
+            <div className="name">IHA Warehouse</div>
+            <div className="count">count</div>
+            <div className="product">product</div>
+          </div>
+        ))}
       </div>
     );
   }
 }
 
-export default CreateQRLabelsPage;
+export default connect(({ organisation }) => ({
+  organisation: organisation
+}))(CreateQRLabelsPage);
