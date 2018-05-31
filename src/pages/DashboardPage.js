71ed19cf6c28598909bd58d2cc9f3ec4f5579cda
Bart Driessen <post@bartdriessen.eu>
2018-05-31 21:44:00 +0300

    Create a first version of the Dashboard Page

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,20 +1,72 @@
 import React from "react";
-import { compose } from "redux";
 import { withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import AppFrame from "../components/AppFrame";
 import Page from "../components/Page";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import PropTypes from "prop-types";
+import Button from "@material-ui/core/Button";
 
-const styles = theme => ({});
+const styles = theme => ({
+  paper: {
+    padding: 16,
+    width: "100%",
+    marginBottom: theme.spacing.unit * 3
+  },
+  button: {
+    marginRight: 20
+  }
+});
 
-const DashboardPage = ({ classes }) => (
-  <AppFrame title="Dashboard">
-    <Page>
-      <Typography variant="body1" gutterBottom>
-        Hello world
-      </Typography>
-    </Page>
-  </AppFrame>
-);
+class DashboardPage extends React.Component {
+  handleChange = key => (event, value) => {
+    this.setState({
+      [key]: value
+    });
+  };
 
-export default compose(withStyles(styles))(DashboardPage);
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <AppFrame title="Dashboard">
+        <Page>
+          <Grid container className={classes.root} xs={12} sm={9} md={7}>
+            <Paper className={classes.paper}>
+              <Typography variant="headline" paragraph="true">
+                Things to do
+              </Typography>
+              <Button
+                variant="raised"
+                color="secondary"
+                className={classes.button}
+                href="/makebox-ui"
+              >
+                Make a box
+              </Button>
+              <Button
+                variant="raised"
+                color="secondary"
+                className={classes.button}
+                href="/findbox-ui"
+              >
+                Find a box
+              </Button>
+            </Paper>
+          </Grid>
+        </Page>
+      </AppFrame>
+    );
+  }
+}
+
+DashboardPage.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(DashboardPage);
