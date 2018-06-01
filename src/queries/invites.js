2cb65dd7d96cbf4ff970551faa605bb036c04a6b
Ben Firshman <ben@firshman.co.uk>
2018-06-01 11:19:46 +0300

    Rename createInvite to addInvite
    Consistency

--- src/queries/invites.js
+++ src/queries/invites.js
@@ -2,7 +2,7 @@ import firebase from "../firebase";
 
 const db = firebase.firestore();
 
-export const createInvite = organizationRef => {
+export const addInvite = organizationRef => {
   return db.collection("invites").add({
     organization: db.doc(organizationRef),
     createdAt: firebase.firestore.FieldValue.serverTimestamp()
@@ -16,7 +16,7 @@ export const getOrAddInvite = organizationRef => {
     .get()
     .then(snapshot => {
       if (snapshot.empty) {
-        return createInvite(organizationRef);
+        return addInvite(organizationRef);
       }
       return Promise.resolve(snapshot.docs[0].ref);
     });
