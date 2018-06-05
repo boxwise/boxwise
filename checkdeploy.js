ef901807463fe37557065dfbed5d4e37f3d09040
Ben Firshman <ben@firshman.co.uk>
2018-06-05 16:41:09 +0300

    Check yarnhook exists before running it

--- checkdeploy.js
+++ checkdeploy.js
@@ -5,20 +5,27 @@ const currentDir = process.cwd();
 const deployfile = "firestore.rules";
 const deployfilePath = join(currentDir, deployfile);
 
-const { stdout: output1 } = execa.sync('yarn',['yarnhook']);
-
-console.log(output1);
+try {
+  const { stdout: output1 } = execa.sync("yarn", ["yarnhook"]);
+  console.log(output1);
+} catch (e) {
+  if (e.message.includes('Command "yarnhook" not found')) {
+    console.error("yarnhook not installed. Run 'yarn'.");
+    process.exit(1);
+  }
+  throw e;
+}
 
 // run a git diff on the deploy file
 const { stdout: output2 } = execa.sync(
-    "git",
-    ["diff", "HEAD@{1}..HEAD@{0}", "--", deployfilePath],
-    { cwd: currentDir }
+  "git",
+  ["diff", "HEAD@{1}..HEAD@{0}", "--", deployfilePath],
+  { cwd: currentDir }
 );
 
 // if diff exists, update dependencies
 if (output2.length > 0) {
-    console.warn(
+  console.warn(
     `Changes to lockfile found, you should run \'yarn run deploy-firestore\' to update the deployment.`
-    );
-} 
+  );
+}
