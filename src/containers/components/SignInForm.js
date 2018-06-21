db0f9f4fb4b0853b802ca419732b422c2a9f41d8
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-06-21 15:21:10 +0200

    Showing sign in errors

--- src/containers/components/SignInForm.js
+++ src/containers/components/SignInForm.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 import { userSignIn } from "../../actions/auth";
 import SignInForm from "../../components/SignInForm";
 
-const mapStateToProps = ({ user: { loading } }) => ({ loading });
+const mapStateToProps = ({ user: { error, loading } }) => ({ error, loading });
 const mapDispatchToProps = { userSignIn };
 export default connect(
   mapStateToProps,
