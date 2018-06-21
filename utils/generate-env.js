6d7b390cd89953c02571edd4cf896c4763202220
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-06-21 16:54:21 +0200

    Config generation now only needs the project ID

--- utils/generate-env.js
+++ utils/generate-env.js
@@ -1,5 +1,6 @@
 const { promisify } = require("util");
 const fs = require("fs");
+const { setup, login } = require("firebase-tools");
 const { resolve } = require("path");
 const prompt = require("prompt");
 const { help, mode, _ } = require("minimist")(process.argv.slice(2));
@@ -12,37 +13,22 @@ const REACT_APP_FIREBASE_AUTH_DOMAIN = "REACT_APP_FIREBASE_AUTH_DOMAIN";
 const REACT_APP_FIREBASE_DATABASE_URL = "REACT_APP_FIREBASE_DATABASE_URL";
 const REACT_APP_FIREBASE_PROJECT_ID = "REACT_APP_FIREBASE_PROJECT_ID";
 const REACT_APP_FIREBASE_STORAGE_BUCKET = "REACT_APP_FIREBASE_STORAGE_BUCKET";
-const REACT_APP_FIREBASE_MESSAGING_SENDER_ID =
-  "REACT_APP_FIREBASE_MESSAGING_SENDER_ID";
+const REACT_APP_FIREBASE_MESSAGING_SENDER_ID = "REACT_APP_FIREBASE_MESSAGING_SENDER_ID";
 const REACT_APP_GOOGLE_ANALYTICS_CODE = "REACT_APP_GOOGLE_ANALYTICS_CODE";
 const REACT_APP_SENTRY_URI = "REACT_APP_SENTRY_URI";
 
 const env = mode || 'local';
 const schema = {
   properties: {
-    [REACT_APP_FIREBASE_API_KEY]: {
-      description: "Firebase API Key"
-    },
-    [REACT_APP_FIREBASE_AUTH_DOMAIN]: {
-      description: "Firebase auth domain"
-    },
-    [REACT_APP_FIREBASE_DATABASE_URL]: {
-      description: "Firebase database url"
-    },
     [REACT_APP_FIREBASE_PROJECT_ID]: {
-      description: "Firebase project ID"
-    },
-    [REACT_APP_FIREBASE_STORAGE_BUCKET]: {
-      description: "Firebase storage bucket"
-    },
-    [REACT_APP_FIREBASE_MESSAGING_SENDER_ID]: {
-      description: "Firebase messaging sender ID"
+      description: "Firebase project ID",
+      required: true
     },
     [REACT_APP_GOOGLE_ANALYTICS_CODE]: {
-      description: "Google Analytics code"
+      description: "[optional] Google Analytics code"
     },
     [REACT_APP_SENTRY_URI]: {
-      description: "Sentry URI"
+      description: "[optional] Sentry URI"
     }
   }
 };
@@ -61,6 +47,7 @@ process.on("SIGINT", () => {
 console.info(`Creating config file of ${env} environment`);
 prompt.start();
 get(schema)
+  .then(getFBConfig)
   .then(createConfig)
   .then(writeConfig)
   .catch(err => console.log(err.message));
@@ -75,4 +62,23 @@ function createConfig(res) {
 function writeConfig(content) {
   const file = resolve(process.cwd(), `.env.${env}`);
   return writeFile(file, content);
+}
+
+function getFBConfig({ REACT_APP_FIREBASE_PROJECT_ID, ...answers }) {
+  console.info("Getting FireBase configuration. This may take a while...");
+  return login()
+    .then(() => setup.web({ project: REACT_APP_FIREBASE_PROJECT_ID }))
+    .then(mapFBConfig)
+    .then(fbConfig => ({ ...fbConfig, ...answers }))
+}
+
+function mapFBConfig({ apiKey, databaseURL, storageBucket, authDomain, messagingSenderId, projectId }) {
+  return {
+    [REACT_APP_FIREBASE_API_KEY]: apiKey,
+    [REACT_APP_FIREBASE_DATABASE_URL]: databaseURL,
+    [REACT_APP_FIREBASE_STORAGE_BUCKET]: storageBucket,
+    [REACT_APP_FIREBASE_AUTH_DOMAIN]: authDomain,
+    [REACT_APP_FIREBASE_MESSAGING_SENDER_ID]: messagingSenderId,
+    [REACT_APP_FIREBASE_PROJECT_ID]: projectId
+  }
 }
