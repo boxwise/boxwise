a4a61afb298bdba311a306c01d29266000c7758f
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-06-25 10:51:36 +0200

    Enabled sourcemaps for production

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,11 @@
-module.exports = function override(config, env) {
-  if (env === 'development') {
-    config.devtool = "source-map";
-  }
-  return config;
-};
+/**
+ * This overrides the default create-react-app webpack config
+ * WARNING: While changing this config stay  concise and create subfunctions to change deep properties
+ * @param {object} config
+ * @param {"production"|"development"|"test"} env
+ * @returns {object}
+ */
+module.exports = (config, env) => ({
+  ...config,
+  devtool: "source-map"
+});
