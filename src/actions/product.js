3d77c4c005df37399012fe8ed07d2d64f1ba95f9
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-06-28 13:48:17 +0200

    Generic API for async actions

--- src/actions/product.js
+++ src/actions/product.js
@@ -1,43 +1,31 @@
 import firebase, { firestore } from "../firebase";
+import {
+  addAction,
+  listAction,
+  editAction,
+  deleteAction
+} from "commons/utils/action-creators";
 import { handleError } from "../utils";
 
-const PRODUCT_LIST_ = TYPE => `PRODUCT_LIST_${TYPE}`;
-const PRODUCT_ADD_ = TYPE => `PRODUCT_ADD_${TYPE}`;
-const PRODUCT_EDIT_ = TYPE => `PRODUCT_EDIT_${TYPE}`;
-const PRODUCT_DELETE_ = TYPE => `PRODUCT_DELETE_${TYPE}`;
-
-export const PRODUCT_LIST_START = PRODUCT_LIST_`START`;
-export const PRODUCT_LIST_SUCCESS = PRODUCT_LIST_`SUCCESS`;
-export const PRODUCT_LIST_ERROR = PRODUCT_LIST_`ERROR`;
-
-export const PRODUCT_ADD_START = PRODUCT_ADD_`START`;
-export const PRODUCT_ADD_SUCCESS = PRODUCT_ADD_`SUCCESS`;
-export const PRODUCT_ADD_ERROR = PRODUCT_ADD_`ERROR`;
-
-export const PRODUCT_EDIT_START = PRODUCT_EDIT_`START`;
-export const PRODUCT_EDIT_SUCCESS = PRODUCT_EDIT_`SUCCESS`;
-export const PRODUCT_EDIT_ERROR = PRODUCT_EDIT_`ERROR`;
-
-export const PRODUCT_DELETE_CONFIRM = PRODUCT_DELETE_`CONFIRM`;
-export const PRODUCT_DELETE_CANCEL = PRODUCT_DELETE_`CANCEL`;
-export const PRODUCT_DELETE_START = PRODUCT_DELETE_`START`;
-export const PRODUCT_DELETE_SUCCESS = PRODUCT_DELETE_`SUCCESS`;
-export const PRODUCT_DELETE_ERROR = PRODUCT_DELETE_`ERROR`;
+export const PRODUCT_LIST = listAction("product");
+export const PRODUCT_ADD = addAction("product");
+export const PRODUCT_EDIT = editAction("product");
+export const PRODUCT_DELETE = deleteAction("product");
 
 // ***** Plain Actions ***** //
 
 export const productDeleteConfirm = productId => ({
-  type: PRODUCT_DELETE_CONFIRM,
+  type: PRODUCT_DELETE.CONFIRM,
   payload: productId
 });
 
-export const productDeleteCancel = () => ({ type: PRODUCT_DELETE_CANCEL });
+export const productDeleteCancel = () => ({ type: PRODUCT_DELETE.CANCEL });
 
 // ***** Thunks ***** //
 
 export const productList = () => (dispatch, getState) => {
   const { organization } = getState().profile.data;
-  dispatch({ type: PRODUCT_LIST_START });
+  dispatch({ type: PRODUCT_LIST.START });
 
   return firestore
     .collection("products")
@@ -47,10 +35,10 @@ export const productList = () => (dispatch, getState) => {
     .orderBy("name", "asc")
     .get()
     .then(({ docs }) => docs.map(doc => ({ id: doc.id, ...doc.data() })))
-    .then(payload => dispatch({ type: PRODUCT_LIST_SUCCESS, payload }))
+    .then(payload => dispatch({ type: PRODUCT_LIST.SUCCESS, payload }))
     .catch(err => {
       handleError(err);
-      dispatch({ type: PRODUCT_LIST_ERROR, payload: err });
+      dispatch({ type: PRODUCT_LIST.ERROR, payload: err });
     });
 };
 
@@ -65,42 +53,42 @@ export const productAdd = product => (dispatch, getState) => {
     isDeleted: false
   };
 
-  dispatch({ type: PRODUCT_ADD_START });
+  dispatch({ type: PRODUCT_ADD.START });
   return firestore
     .collection("products")
     .add(values)
     .then(ref => ref.get())
-    .then(res => dispatch({ type: PRODUCT_ADD_SUCCESS, payload: res.data() }))
+    .then(res => dispatch({ type: PRODUCT_ADD.SUCCESS, payload: res.data() }))
     .catch(err => {
       handleError(err); // TODO: actually handle the error
-      dispatch({ type: PRODUCT_ADD_ERROR, payload: err });
+      dispatch({ type: PRODUCT_ADD.ERROR, payload: err });
     });
 };
 
 export const productEdit = product => dispatch => {
-  dispatch({ type: PRODUCT_EDIT_START });
+  dispatch({ type: PRODUCT_EDIT.START });
   const ref = firestore.collection("products").doc(product.id);
 
   return ref
     .update(product)
     .then(() => ref.get())
-    .then(res => dispatch({ type: PRODUCT_EDIT_SUCCESS, payload: res.data() }))
+    .then(res => dispatch({ type: PRODUCT_EDIT.SUCCESS, payload: res.data() }))
     .catch(err => {
       handleError(err); // TODO: actually handle the error
-      dispatch({ type: PRODUCT_EDIT_ERROR, payload: err });
+      dispatch({ type: PRODUCT_EDIT.ERROR, payload: err });
     });
 };
 
 export const productDelete = productId => dispatch => {
-  dispatch({ type: PRODUCT_DELETE_START });
+  dispatch({ type: PRODUCT_DELETE.START });
 
   return firestore
     .collection("products")
     .doc(productId)
     .update({ isDeleted: true })
-    .then(() => dispatch({ type: PRODUCT_DELETE_SUCCESS }))
+    .then(() => dispatch({ type: PRODUCT_DELETE.SUCCESS }))
     .catch(err => {
       handleError(err);
-      dispatch({ type: PRODUCT_DELETE_ERROR });
+      dispatch({ type: PRODUCT_DELETE.ERROR });
     });
 };
--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,17 +1,8 @@
 import {
-  PRODUCT_DELETE_START,
-  PRODUCT_DELETE_SUCCESS,
-  PRODUCT_DELETE_ERROR,
-  PRODUCT_DELETE_CONFIRM,
-  PRODUCT_DELETE_CANCEL,
-  PRODUCT_LIST_START,
-  PRODUCT_EDIT_START,
-  PRODUCT_EDIT_SUCCESS,
-  PRODUCT_LIST_ERROR,
-  PRODUCT_EDIT_ERROR,
-  PRODUCT_LIST_SUCCESS,
-  PRODUCT_ADD_SUCCESS,
-  PRODUCT_ADD_START
+  PRODUCT_DELETE,
+  PRODUCT_LIST,
+  PRODUCT_EDIT,
+  PRODUCT_ADD
 } from "../actions/product";
 
 export default function products(
@@ -24,36 +15,36 @@ export default function products(
   { type, payload }
 ) {
   switch (type) {
-    case PRODUCT_ADD_SUCCESS:
+    case PRODUCT_ADD.SUCCESS:
       return { ...state, loading: false, data: [payload, ...state.data] };
 
-    case PRODUCT_LIST_SUCCESS:
+    case PRODUCT_LIST.SUCCESS:
       return { ...state, data: payload, loading: false };
 
-    case PRODUCT_EDIT_SUCCESS:
+    case PRODUCT_EDIT.SUCCESS:
       return {
         ...state,
         loading: false,
         data: state.data.map(prod => (prod.id === payload.id ? payload : prod))
       };
 
-    case PRODUCT_LIST_ERROR:
-    case PRODUCT_EDIT_ERROR:
+    case PRODUCT_LIST.ERROR:
+    case PRODUCT_EDIT.ERROR:
       return { ...state, error: payload };
 
-    case PRODUCT_DELETE_CONFIRM:
+    case PRODUCT_DELETE.CONFIRM:
       return { ...state, confirmDeleteOf: payload };
 
-    case PRODUCT_DELETE_CANCEL:
+    case PRODUCT_DELETE.CANCEL:
       return { ...state, confirmDeleteOf: null };
 
-    case PRODUCT_ADD_START:
-    case PRODUCT_LIST_START:
-    case PRODUCT_EDIT_START:
-    case PRODUCT_DELETE_START:
+    case PRODUCT_ADD.START:
+    case PRODUCT_LIST.START:
+    case PRODUCT_EDIT.START:
+    case PRODUCT_DELETE.START:
       return { ...state, loading: true };
 
-    case PRODUCT_DELETE_SUCCESS:
+    case PRODUCT_DELETE.SUCCESS:
       return {
         ...state,
         confirmDeleteOf: null,
@@ -61,7 +52,7 @@ export default function products(
         data: state.data.filter(prod => prod.id !== state.confirmDeleteOf)
       };
 
-    case PRODUCT_DELETE_ERROR:
+    case PRODUCT_DELETE.ERROR:
       return {
         ...state,
         loading: false,
