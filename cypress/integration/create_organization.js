b98fc8e433f12c76b3bf941fd29de8ec48442dff
Ben Firshman <ben@firshman.co.uk>
2018-07-09 20:10:10 +0300

    Add start of a real create organization test

--- cypress/integration/create_organization.js
+++ cypress/integration/create_organization.js
@@ -1,6 +1,14 @@
-describe('Add Organization', function() {
-    it('Adds an organization', function() {
-        cy.visit('http://localhost:3000/create-organization')
-        // TODO: finish creating an organization
-    })
-})
+import uuidv4 from "uuid/v4";
+import firebase from "firebase";
+
+describe("Create organization flow", function() {
+  it("creates an organization", function() {
+    cy.visit("http://localhost:3000/signout");
+    cy.visit("http://localhost:3000/create-organization");
+    const organizationName = uuidv4();
+    cy.get("input[name=name]").type(`${organizationName}{enter}`);
+    cy.get("input[name=name]").type("Alice Test");
+    cy.get("input[name=email]").type(`${organizationName}@example.com`);
+    cy.get("input[name=password]").type("hunter2{enter}");
+  });
+});
