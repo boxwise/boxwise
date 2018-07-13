588825474def8b4e99de305790e250023d9de870
Rubens Pinheiro <rubenspgcavalcante@gmail.com>
2018-07-13 09:41:51 +0200

    Cleaning the actions using the async actions factory

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,24 +1,14 @@
 import firebase from "../firebase";
 import { handleError } from "../utils";
 import { fetchProfile } from "./profile";
+import { asyncAction, editAction } from "commons/utils/action-creators";
 
-export const USER_NOT_LOGGED = "USER_NOT_LOGGED";
+export const USER_SIGN_IN = asyncAction("USER_SIGN_IN");
+export const USER_NOT_SIGN_IN = "USER_NOT_LOGGED";
+export const USER_SIGN_OUT = "USER_SIGN_OUT";
 
-const USER_SIGN_ = TYPE => `USER_SIGN_IN_${TYPE}`;
-export const USER_SIGN_IN_START = USER_SIGN_`IN_START`;
-export const USER_SIGN_IN_SUCCESS = USER_SIGN_`IN_SUCCESS`;
-export const USER_SIGN_ERROR = USER_SIGN_`ERROR`;
-export const USER_SIGN_OUT = USER_SIGN_`OUT`;
-
-const PASSWORD_RESET_ = TYPE => `PASSWORD_RESET_${TYPE}`;
-export const PASSWORD_RESET_START = PASSWORD_RESET_`START`;
-export const PASSWORD_RESET_SUCCESS = PASSWORD_RESET_`SUCCESS`;
-export const PASSWORD_RESET_ERROR = PASSWORD_RESET_`ERROR`;
-
-const PASSWORD_CHANGE_ = TYPE => `USER_PASSWORD_CHANGE_${TYPE}`;
-export const PASSWORD_CHANGE_START = PASSWORD_CHANGE_`START`;
-export const PASSWORD_CHANGE_SUCCESS = PASSWORD_CHANGE_`SUCCESS`;
-export const PASSWORD_CHANGE_ERROR = PASSWORD_CHANGE_`ERROR`;
+export const PASSWORD_RESET = asyncAction("PASSWORD_RESET");
+export const PASSWORD_EDIT = editAction("password");
 
 export const userSignOut = () => ({ type: USER_SIGN_OUT });
 
@@ -27,36 +17,36 @@ export const firebaseSignOut = () => dispatch => {
     .auth()
     .signOut()
     .then(() => dispatch(userSignOut()))
-    .catch(err => dispatch({ type: USER_SIGN_ERROR, payload: err }));
+    .catch(err => dispatch({ type: USER_SIGN_IN.ERROR, payload: err }));
 };
 
 export const userSignInSuccess = user => dispatch => {
   const payload = user.toJSON();
-  dispatch({ type: USER_SIGN_IN_SUCCESS, payload });
+  dispatch({ type: USER_SIGN_IN.SUCCESS, payload });
   dispatch(fetchProfile(payload.uid));
 };
 
 export const userSignIn = ({ email, password }) => dispatch => {
-  dispatch({ type: USER_SIGN_IN_START });
+  dispatch({ type: USER_SIGN_IN.START });
   firebase
     .auth()
     .signInWithEmailAndPassword(email, password)
     .then(({ user }) => dispatch(userSignInSuccess(user)))
     .catch(error => {
       handleError(error);
-      dispatch({ type: USER_SIGN_ERROR, payload: error });
+      dispatch({ type: USER_SIGN_IN.ERROR, payload: error });
     });
 };
 
 export const resetPassword = ({ email }) => dispatch => {
-  dispatch({ type: PASSWORD_RESET_START });
+  dispatch({ type: PASSWORD_RESET.START });
   firebase
     .auth()
     .sendPasswordResetEmail(email)
-    .then(() => dispatch({ type: PASSWORD_RESET_SUCCESS }))
+    .then(() => dispatch({ type: PASSWORD_RESET.SUCCESS }))
     .catch(error => {
       handleError(error);
-      dispatch({ type: PASSWORD_RESET_ERROR, payload: error });
+      dispatch({ type: PASSWORD_RESET.ERROR, payload: error });
     });
 };
 
@@ -65,23 +55,23 @@ export const userPasswordChange = ({
   currentPassword,
   newPassword
 }) => dispatch => {
-  dispatch({ type: PASSWORD_CHANGE_START });
+  dispatch({ type: PASSWORD_EDIT.START });
   return firebase
     .auth()
     .signInWithEmailAndPassword(email, currentPassword)
     .then(({ user }) =>
       user
         .updatePassword(newPassword)
-        .then(() => dispatch({ type: PASSWORD_CHANGE_SUCCESS }))
+        .then(() => dispatch({ type: PASSWORD_EDIT.SUCCESS }))
         .catch(error => {
           handleError(error);
-          dispatch({ type: PASSWORD_CHANGE_ERROR, payload: error });
+          dispatch({ type: PASSWORD_EDIT.ERROR, payload: error });
           throw error;
         })
     )
     .catch(error => {
       handleError(error);
-      dispatch({ type: PASSWORD_CHANGE_ERROR, payload: error });
+      dispatch({ type: PASSWORD_EDIT.ERROR, payload: error });
       throw error;
     });
 };
@@ -89,11 +79,11 @@ export const userPasswordChange = ({
 export const registerAuthStateObserver = () => (dispatch, getState) => {
   const { loading } = getState().user;
   if (loading === null) {
-    dispatch({ type: USER_SIGN_IN_START });
+    dispatch({ type: USER_SIGN_IN.START });
 
     return firebase.auth().onAuthStateChanged(user => {
       if (!user) {
-        dispatch({ type: USER_NOT_LOGGED });
+        dispatch({ type: USER_NOT_SIGN_IN });
         userSignOut();
       } else dispatch(userSignInSuccess(user));
     });
--- src/commons/utils/action-creators.js
+++ src/commons/utils/action-creators.js
@@ -1,9 +1,14 @@
-export const asyncAction = name => ({
+export const asyncAction = (name, extras = []) => ({
   START: `${name}_START`,
   SUCCESS: `${name}_SUCCESS`,
   ERROR: `${name}_ERROR`,
   CONFIRM: `${name}_CONFIRM`,
-  CANCEL: `${name}_CANCEL`
+  CANCEL: `${name}_CANCEL`,
+  ...extras.reduce((prev, curr) => {
+    const key = curr.toUpperCase();
+    prev[key] = `${name}_${key}`;
+    return prev;
+  }, {})
 });
 
 export const [addAction, listAction, findAction, editAction, deleteAction] = [
