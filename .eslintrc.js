23a58ee3469e025b85fbe40122a2d0b4b622918b
James Crowley <james.crowley@gmail.com>
2019-04-21 10:00:41 +0200

    Refactor ESLint configuration so it's clearer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
+const denyImportPatterns = {
+  // rule: don't allow imports of modules
+  anyModules: "modules/**",
+  // rule: don't allow imports of internal module files
+  anyModuleInternals: "modules/*/*/**",
+  // rule: don't allow relative imports that go up the tree
+  relativeImportsUpAnyLevel: "../**",
+  // rule: don't allow relative imports that go up the tree more than one level
+  relativeImportsUpMoreThanOneLevel: "../../**"
+}
+
 module.exports = {
   "plugins": ["eslint-plugin-import","react-hooks"],
   "extends": ["react-app","plugin:import/recommended"],
@@ -10,16 +21,12 @@ module.exports = {
     "react-hooks/exhaustive-deps": "error",
     "no-restricted-imports": [ "error",
       {
+        "paths": [],
         "patterns": [
-          /* 
-            rule: don't allow relative imports that go up the tree more than one level
-            why: preventing weird hierarchies for files */
-          "../../**",
-          /* 
-            rule: don't allow imports of internal module files
-            why: you should use the public 'API' exposed by the module
-           */
-          "modules/*/*/**"
+          // why: preventing weird hierarchies for files */
+          denyImportPatterns.relativeImportsUpMoreThanOneLevel,
+          // why: you should use the public 'API' exposed by the module
+          denyImportPatterns.anyModuleInternals
         ]
       }
     ]
@@ -30,18 +37,14 @@ module.exports = {
       "rules": {
         "no-restricted-imports": [ "error",
           {
-            /* rule: don't allow use of redux 
-               why: these components should be self-contained */
+            // rule: don't allow use of redux 
+            // why: these components should be self-contained
             "paths": ["react-redux"],
             "patterns": [
-              /* 
-                rule: don't allow relative imports that go up the tree
-                why: these components should be self-contained */
-              "../**",
-              /* 
-                rule: don't allow imports of modules
-                why: these components should be standalone */
-              "modules/**"
+              // why: these components should be self-contained
+              denyImportPatterns.relativeImportsUpAnyLevel,
+              // why: these components should be standalone
+              denyImportPatterns.anyModules
             ]
           }
         ]
@@ -53,18 +56,16 @@ module.exports = {
         "no-restricted-imports": [ 
           "error", 
           {
-            /* rule: don't allow use of redux 
-               why: these are unconnected components that shouldn't
-                    be hooked up to redux. use containers instead. */
+            // rule: don't allow use of redux 
+            // why: these are unconnected components that shouldn't
+            //      be hooked up to redux. use containers instead.
             "paths": ["react-redux"],
             "patterns": [
-              /* 
-                rule: don't allow relative imports that go up the tree more than one level 
-                why: preventing weird hierarchies for files*/
-              "../../**",
-              /* rule: don't allow imports of internal modules 
-                why: you should use the public 'API' exposed by the module */
-              "modules/*/*/**" ]
+              // why: preventing weird hierarchies for files
+              denyImportPatterns.relativeImportsUpMoreThanOneLevel,
+              // why: you should use the public 'API' exposed by the module
+              denyImportPatterns.anyModuleInternals
+            ]
           }
         ]
       }
