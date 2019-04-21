0f2c47d1dca8874b4d5f4edce2683e5b34f00895
James Crowley <james.crowley@gmail.com>
2019-04-21 08:32:32 +0200

    Config is not a component, so switch it to lowercase. This is a two-step commit to ensure the file casing is changed.

--- src/config-1.js
+++ src/config-1.js
@@ -1,6 +1,4 @@
-const Config = {
+export default {
   GOOGLE_ANALYTICS_CODE: process.env.REACT_APP_GOOGLE_ANALYTICS_CODE,
   SENTRY_URI: process.env.REACT_APP_SENTRY_URI
 };
-
-export default Config;
