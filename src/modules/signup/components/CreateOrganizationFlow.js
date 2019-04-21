5bcdaf78593adeb32848d344f78d7f4adea45d4f
James Crowley <james.crowley@gmail.com>
2019-04-21 09:04:13 +0200

    Remove auth helper, as it's to do with the sign up process

--- src/modules/signup/components/CreateOrganizationFlow.js
+++ src/modules/signup/components/CreateOrganizationFlow.js
@@ -2,10 +2,9 @@ import React from "react";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 
-import { createUserAndProfile } from "auth";
 import { handleError } from "utils";
 
-import { addOrganization } from "../actions";
+import { addOrganization, createUserAndProfile } from "../actions";
 import InviteLink from "../containers/InviteLink";
 
 import CreateOrganizationForm from "./CreateOrganizationForm";
