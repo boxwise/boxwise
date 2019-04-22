28c6c23c58aedaa24bc60d187924415a84700433
James Crowley <james.crowley@gmail.com>
2019-04-22 23:04:57 +0200

    Remove Formik from ResetPasswordPage.

--- src/modules/auth/containers/ResetPasswordForm.jsx
+++ src/modules/auth/containers/ResetPasswordForm.jsx
@@ -3,7 +3,7 @@ import { connect } from "react-redux";
 import { resetPassword } from "../actions";
 import ResetPasswordForm from "../components/ResetPasswordForm";
 
-const mapStateToProps = ({ user: { loading } }) => ({ loading });
+const mapStateToProps = ({ user: { loading, error } }) => ({ loading, error });
 const mapDispatchToProps = { resetPassword };
 export default connect(
   mapStateToProps,
