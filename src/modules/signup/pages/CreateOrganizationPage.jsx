c5ea8e0c80449a51b4f1664fe79e7e90dc1b8d7f
James Crowley <james.crowley@gmail.com>
2019-05-04 18:29:36 +0200

    fix: Layout for sign up flow - current styles don't push the text down below the AppBarHeader component. For now using the same 'cheat' the main layout uses, with an empty toolbar the same size.

--- src/modules/signup/pages/CreateOrganizationPage.jsx
+++ src/modules/signup/pages/CreateOrganizationPage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
 import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
 
 import { Page } from "modules/layout/components";
 import AppBarHeader from "components/AppBarHeader";
@@ -11,10 +12,9 @@ export const CreateOrganizationPage = ({ history }) => (
   <React.Fragment>
     <AppBarHeader backUrl="/signin" title="Set up Boxwise" />
     <Page>
+      <Toolbar />
       <Grid container spacing={24} justify="center" alignItems="center">
         <Grid item xs={12} md={4}>
-          <br />
-          <br />
           <CreateOrganizationFlow
             onDone={() => {
               history.push("/");
