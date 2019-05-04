0f7ba94b36635f6207ba4fd76803224995636388
James Crowley <james.crowley@gmail.com>
2019-05-04 13:34:40 +0200

    Hook up additional sentry middleware so we get redux action breadcrumbs recorded

--- src/errorHandling.js
+++ src/errorHandling.js
@@ -3,9 +3,33 @@ import * as Sentry from "@sentry/browser";
 // eslint-disable-next-line import/prefer-default-export
 export function captureException(error, errorInfo) {
   // eslint-disable-next-line no-console
-  console.error(error);
+  console.error(error, errorInfo);
   Sentry.captureException(error, { extra: errorInfo });
 }
+
+// copied from https://github.com/captbaritone/raven-for-redux/issues/93#issuecomment-435854873
+export const sentryMiddleware = store => {
+  Sentry.addGlobalEventProcessor(event => {
+    const state = store.getState();
+
+    return {
+      ...event,
+      extra: {
+        ...event.extra,
+        "redux:state": state
+      }
+    };
+  });
+
+  return next => action => {
+    Sentry.addBreadcrumb({
+      category: "redux-action",
+      message: action.type
+    });
+
+    return next(action);
+  };
+};
 
 export const logErrorActionsAsExceptions = () => next => action => {
   if (action.type.endsWith("_ERROR")) {
