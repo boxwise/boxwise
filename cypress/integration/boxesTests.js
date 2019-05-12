3314f4282a51a785f6335e1ae326d41a6a0a4b8a
James Crowley <james.crowley@gmail.com>
2019-05-12 21:01:56 +0200

    Apply linting rules to Cypress tests (#480)
    * Update Lint configuration to include cypress files
    
    * Apply automatic linting fixes
    
    * Remove hard-coded localhost URLs and use the pre-defined Cypress environment variable described here: https://docs.cypress.io/guides/guides/environment-variables.html#Setting
    
    * Manual updates for linting. Pull out the environment variable config (triggered by a linting warning to rewrite using a deconstructor anyway) as that can just be read directly with normal code.
    
    * cs.wait() not allowed by linting rules - should see if there's a nicer way to get these running reliably?
    
    * Update travis to run cypress via yarn (so it uses the script shortcuts in package.json) - and sets the environment variable of CYPRESS_baseUrl=http://localhost:3000 for now
    
    * For now, ignore the wait() warnings so we can get the rest of the linting fixes in

--- cypress/integration/boxesTests.js
+++ cypress/integration/boxesTests.js
@@ -1,69 +1,84 @@
 import uuidv4 from "uuid/v4";
+import { getTestConfig } from "../config";
 
-describe('CRUD boxes tests', function() {
-    let testUserMail;
-    let testPwd;
+describe("CRUD boxes tests", () => {
+  const { testUserMail, testPwd } = getTestConfig();
 
-    before(function() {
-        cy.getTestData().then(($result) => {
-            testUserMail = $result.testUserMail;
-            testPwd = $result.testPwd;
-        });
-    })
+  beforeEach(() => {
+    cy.reLogin(testUserMail, testPwd);
+  });
 
-    beforeEach(function() {        
-        cy.reLogin(testUserMail, testPwd);
-    });
+  it("Create new box", () => {
+    const testCount = Math.floor(Math.random() * 6) + 1;
+    const testComment = uuidv4().substring(0, 6);
+    cy.get("button[data-cy=makeBoxButton]").click();
+    cy.get("div[data-cy=selectProduct]").click();
+    cy.get("form[data-cy=boxForm]").should("exist"); // form to add box should pop up
+    cy.get("li[data-cy=productDropdownItem]")
+      .first()
+      .click();
+    cy.get("div[data-cy=quantity] input").type(`${testCount}`);
+    cy.get("div[data-cy=comment] textarea").type(`${testComment}`);
+    cy.get("button[data-cy=submitCreateBox]")
+      .click({ timeout: 10000 })
+      .then(() => {
+        cy.get("h6[data-cy='boxCreatedLabel']").should("exist"); // "Box created" label with box details should be visible
+      });
+  });
 
-    it('Create new box', () => {
-        let testCount = Math.floor(Math.random() * 6) + 1;
-        let testComment = uuidv4().substring(0,6);
-        cy.get('button[data-cy=makeBoxButton]').click();
-        cy.get('div[data-cy=selectProduct]').click();
-        cy.get('form[data-cy=boxForm]').should('exist');      //form to add box should pop up
-        cy.get('li[data-cy=productDropdownItem]').first().click();
-        cy.get('div[data-cy=quantity] input').type(`${testCount}`);
-        cy.get('div[data-cy=comment] textarea').type(`${testComment}`);
-        cy.get('button[data-cy=submitCreateBox]').click({ timeout: 10000 }).then(() => {
-            cy.get("h6[data-cy='boxCreatedLabel']").should('exist');  //"Box created" label with box details should be visible
-        });            
-    });
+  it("Create 2 boxes in a row updates overview of last added box", () => {
+    const testCount = Math.floor(Math.random() * 6) + 1;
+    const testComment = uuidv4().substring(0, 6);
+    cy.get("button[data-cy=makeBoxButton]").click();
+    cy.get("div[data-cy=selectProduct]").click();
+    cy.get("form[data-cy=boxForm]").should("exist"); // form to add box should pop up
+    cy.get("li[data-cy=productDropdownItem]")
+      .first()
+      .click();
+    cy.get("div[data-cy=quantity] input").type(`${testCount}`);
+    cy.get("div[data-cy=comment] textarea").type(`${testComment}`);
+    cy.get("button[data-cy=submitCreateBox]")
+      .click({ timeout: 10000 })
+      .then(() => {
+        cy.get("h6[data-cy='boxCreatedLabel']").should("exist"); // "Box created" label with box details should be visible
+        cy.get("h6[data-cy='boxCreatedQuantityLabel']").should(
+          "contain",
+          `${testCount}x`
+        ); // count of items should be displayed
+        cy.get("button[data-cy='createAnotherBoxButton']").click();
+        cy.get("div[data-cy=selectProduct]").click();
+        cy.get("form[data-cy=boxForm]").should("exist"); // form to add box should pop up
+        cy.get("li[data-cy=productDropdownItem]")
+          .first()
+          .click();
+        cy.get("div[data-cy=quantity] input").type(`${testCount + 1}`);
+        cy.get("div[data-cy=comment] textarea").type(`${testComment}`);
+        cy.get("button[data-cy=submitCreateBox]")
+          .click({ timeout: 10000 })
+          .then(() => {
+            cy.get("h6[data-cy='boxCreatedLabel']").should("exist"); // "Box created" label with box details should be visible
+            cy.get("h6[data-cy='boxCreatedQuantityLabel']").should(
+              "contain",
+              `${testCount + 1}x`
+            ); // updated count of items should be displayed
+          });
+      });
+  });
 
-    it('Create 2 boxes in a row updates overview of last added box', () => {
-        let testCount = Math.floor(Math.random() * 6) + 1;
-        let testComment = uuidv4().substring(0,6);
-        cy.get('button[data-cy=makeBoxButton]').click();
-        cy.get('div[data-cy=selectProduct]').click();
-        cy.get('form[data-cy=boxForm]').should('exist');      //form to add box should pop up
-        cy.get('li[data-cy=productDropdownItem]').first().click();
-        cy.get('div[data-cy=quantity] input').type(`${testCount}`);
-        cy.get('div[data-cy=comment] textarea').type(`${testComment}`);
-        cy.get('button[data-cy=submitCreateBox]').click({ timeout: 10000 }).then(() => {
-            cy.get("h6[data-cy='boxCreatedLabel']").should('exist');  //"Box created" label with box details should be visible
-            cy.get("h6[data-cy='boxCreatedQuantityLabel']").should('contain', `${testCount}x`);   //count of items should be displayed
-            cy.get("button[data-cy='createAnotherBoxButton']").click();
-            cy.get('div[data-cy=selectProduct]').click();
-            cy.get('form[data-cy=boxForm]').should('exist');      //form to add box should pop up
-            cy.get('li[data-cy=productDropdownItem]').first().click();
-            cy.get('div[data-cy=quantity] input').type(`${testCount+1}`);
-            cy.get('div[data-cy=comment] textarea').type(`${testComment}`);
-            cy.get('button[data-cy=submitCreateBox]').click({ timeout: 10000 }).then(() => {
-                cy.get("h6[data-cy='boxCreatedLabel']").should('exist');   //"Box created" label with box details should be visible
-                cy.get("h6[data-cy='boxCreatedQuantityLabel']").should('contain', `${testCount+1}x`);  //updated count of items should be displayed
-            });
-        });         
-    });
-
-    it('Number of items during box creation has to be specified', () => {
-        let testComment = uuidv4().substring(0,6);
-        cy.get('button[data-cy=makeBoxButton]').should('be.visible');
-        cy.get('button[data-cy=makeBoxButton]').click();
-        cy.get('div[data-cy=selectProduct]').click();
-        cy.get('form[data-cy=boxForm]').should('exist');      //form to add box should pop up
-        cy.get('li[data-cy=productDropdownItem]').first().click();
-        cy.get('div[data-cy=comment] textarea').type(`${testComment}`);
-        cy.get('button[data-cy=submitCreateBox]').click({ timeout: 10000 }).then(() => {
-            cy.get('div[data-cy=quantity] input').should('exist');  //number of items input should be visible
-        });            
-    });
+  it("Number of items during box creation has to be specified", () => {
+    const testComment = uuidv4().substring(0, 6);
+    cy.get("button[data-cy=makeBoxButton]").should("be.visible");
+    cy.get("button[data-cy=makeBoxButton]").click();
+    cy.get("div[data-cy=selectProduct]").click();
+    cy.get("form[data-cy=boxForm]").should("exist"); // form to add box should pop up
+    cy.get("li[data-cy=productDropdownItem]")
+      .first()
+      .click();
+    cy.get("div[data-cy=comment] textarea").type(`${testComment}`);
+    cy.get("button[data-cy=submitCreateBox]")
+      .click({ timeout: 10000 })
+      .then(() => {
+        cy.get("div[data-cy=quantity] input").should("exist"); // number of items input should be visible
+      });
+  });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,64 +25,60 @@ import uuidv4 from "uuid/v4";
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
-Cypress.Commands.add("getTestData", () => { 
-    let testUser = Cypress.env('testUser');
-    let testUserMail = Cypress.env('testUserMail');
-    let testPwd = Cypress.env('testPwd');
-    return {testUser: testUser, testUserMail: testUserMail, testPwd: testPwd};
+Cypress.Commands.add("reLogin", (userMail, userPassword) => {
+  cy.visit("/signout");
+  cy.visit("/signin");
+  cy.get("div[data-cy=email] input").type(`${userMail}`);
+  cy.get("div[data-cy=password] input").type(`${userPassword}`);
+  cy.get("button[data-cy=signInButton]").click();
 });
 
-Cypress.Commands.add("getChangePwdData", () => {
-    let changePwdUserMail = Cypress.env('changePwdUserMail');
-    let changePwdPwd = Cypress.env('changePwdPwd');
-    let newPwd = Cypress.env('newPwd');
-    return {changePwdUserMail: changePwdUserMail, changePwdPwd: changePwdPwd, newPwd: newPwd};
-});
-
-Cypress.Commands.add("reLogin", (userMail, userPassword) => { 
-    cy.visit("http://localhost:3000/signout");
-    cy.visit("http://localhost:3000/signin");
-    cy.get("div[data-cy=email] input").type(`${userMail}`);
-    cy.get("div[data-cy=password] input").type(`${userPassword}`);
-    cy.get("button[data-cy=signInButton]").click();
-});
-
-Cypress.Commands.add("openAppDrawer", () => { 
-    cy.get("button[data-cy=appDrawerOpener]").click();
-    cy.get("div[data-cy=appDrawerDiv]").should('be.visible');
+Cypress.Commands.add("openAppDrawer", () => {
+  cy.get("button[data-cy=appDrawerOpener]").click();
+  cy.get("div[data-cy=appDrawerDiv]").should("be.visible");
 });
 
 Cypress.Commands.add("navigateToChangePasswordForm", () => {
-    cy.openAppDrawer();
-    cy.get("a[data-cy=changePasswordDrawerButton]").last().click();
+  cy.openAppDrawer();
+  cy.get("a[data-cy=changePasswordDrawerButton]")
+    .last()
+    .click();
 });
 
-
 Cypress.Commands.add("navigateToProductsPage", () => {
-    cy.openAppDrawer();
-    cy.get("a[data-cy=productsDrawerButton]").last().click();
+  cy.openAppDrawer();
+  cy.get("a[data-cy=productsDrawerButton]")
+    .last()
+    .click();
 });
 
 Cypress.Commands.add("navigateToInvitePage", () => {
-    cy.openAppDrawer();
-    cy.get("a[data-cy=inviteDrawerButton]").last().click();
+  cy.openAppDrawer();
+  cy.get("a[data-cy=inviteDrawerButton]")
+    .last()
+    .click();
 });
 
 // 'Delete product' helper function
 Cypress.Commands.add("createTestProduct", () => {
-    let productName = uuidv4().substring(0,6);
-    cy.get('button[data-cy=addProductButton]').click();
-    cy.get('div[data-cy=selectCategory]').click();
-    cy.get('li[id=category]').first().click();
-    cy.get('div[data-cy=productName] input').type(`${productName}`);
-    cy.get('button[data-cy=submitCreateProduct').click({ timeout: 10000 });
-    cy.wait(2000);   //give table some time to update (without wait or doing this in click().then({}) doesn't find new row at all)
-    cy.get('td[data-cy=productNameCell]').contains(`${productName}`).should('exist');  //cell with product name should be visible
+  const productName = uuidv4().substring(0, 6);
+  cy.get("button[data-cy=addProductButton]").click();
+  cy.get("div[data-cy=selectCategory]").click();
+  cy.get("li[id=category]")
+    .first()
+    .click();
+  cy.get("div[data-cy=productName] input").type(`${productName}`);
+  cy.get("button[data-cy=submitCreateProduct").click({ timeout: 10000 });
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(2000); // give table some time to update (without wait or doing this in click().then({}) doesn't find new row at all)
+  cy.get("td[data-cy=productNameCell]")
+    .contains(`${productName}`)
+    .should("exist"); // cell with product name should be visible
 });
 
-//https://github.com/cypress-io/cypress/issues/761
-Cypress.Commands.add("cleanUpXHR", function() {
-    if (Cypress.env("run_all_suite")) {
-      cy.visit(`${Cypress.env("yourAppUrl")}/404`, { failOnStatusCode: false });
-    }
+// https://github.com/cypress-io/cypress/issues/761
+Cypress.Commands.add("cleanUpXHR", () => {
+  if (Cypress.env("run_all_suite")) {
+    cy.visit("/404", { failOnStatusCode: false });
+  }
 });
